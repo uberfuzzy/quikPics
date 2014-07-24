b14ae0642f532d50f1dd7072b2d51df0f1a01df8
Uberfuzzy <i.am@uberfuzzy.com>
2014-07-24 19:32:18 -0400

    code style, comment style, spacing, indenting
    srsly, what did i write this with?
    oh yeah, MS notepad, in 2007

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php
 $ppath = "http://".$_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-?><html>
+?><!DOCTYPE html>
+<html>
 <head>
 <title>ubrfzy's quick image linker</title>
 <style>
-body{ color: white; background: black; }
-input.lnkbox{ padding: 0px 0px 0px 10px; font-size: 10px;}
+body { color: white; background: black; }
+input.lnkbox { padding: 0px 0px 0px 10px; font-size: 10px; }
 span.fil {}
-span.siz, input.lnkbox { font-family: monospace; font-size: 12px;}
+span.siz, input.lnkbox { font-family: monospace; font-size: 12px; }
 </style>
 </head>
 <body>
-<?
-/************SETTINGS**********/
+<?php
+#************SETTINGS**********
 $perrow = 5;
 $maxw = 150;
 $minh = 50;
-/********************************/
+#******************************
 
 $raw = glob("*.*");
 
@@ -25,147 +26,133 @@ unset($raw);
 
 print "<br>\n";
 
-if($passc == 0)
-{
-print "<center>";
-	print "folder empty";
-print "</center>";
-}
-else
-{
-/******************* BEGIN MAIN LOGIC /***************/
-print "<center>";
-print "<table border=0 cellpadding=5 cellspacing=0>\n";
+if($passc == 0) {
+	print "<center>";
+		print "folder empty";
+	print "</center>";
+} else {
+	#******************* BEGIN MAIN LOGIC ***************
+	print "<center>";
+	print "<table border=0 cellpadding=5 cellspacing=0>\n";
 
-$cell = 0;
-foreach($clean as $fil)
-{
-	if( ($cell % 4) == 0)
-	{print "<tr>\n";}
+	$cell = 0;
+	foreach( $clean as $fil ) {
+		if( ($cell % 4) == 0 ) {
+			print "<tr>\n";
+		}
 
-	print "<td align=\"center\" valign=\"top\"><div>";
+		print "<td align=\"center\" valign=\"top\"><div>";
 
-	$data = getimagesize($fil);
-	$rat = ($data[0]/$data[1]);
-		//echo "data=/".$data[0]."&times;".$data[1]."/<br>";
-		//echo "rat=/$rat/<br>";
+		$data = getimagesize($fil);
+		$rat = ($data[0]/$data[1]);
+			//echo "data=/".$data[0]."&times;".$data[1]."/<br>";
+			//echo "rat=/$rat/<br>";
 
-	if($data[0] > $maxw)
-	{
-		$w = $maxw;
-		$h = floor($maxw / $rat);
-		//echo "h/$h/<br>\n";
+		if( $data[0] > $maxw ) {
+			$w = $maxw;
+			$h = floor($maxw / $rat);
+			//echo "h/$h/<br>\n";
+		} else {
+			$w = $data[0];
+		}
+
+		if( $h < $minh ) {
+			$h = $minh;
+			$w = floor($h * $rat);
+			if( $w > 300 ) {
+				$w=300;
+			}
+		} else {
+
+		}
+
+		#get some info prepared for use
+		$fulfil = ($ppath.$fil);
+		$sz = size_readable (filesize($fil));
+
+		#print the name at the top
+		print "<span class=\"fil\">$fil</span><br>\n";
+
+		#*****not modded  WxH****
+		print "<span class=\"siz\">{$data[0]}&times;{$data[1]}</span><br>\n";
+		//print $sz . "<br>\n";
+
+		print "</div>\n";
+
+		print "raw <input type=text value=\"".$fulfil."\" size=10 class=\"lnkbox\"><br>\n";
+		print 'link <input type=text value="'."<a href='{$fulfil}'>text</a>".'" size=10 class="lnkbox">'."<br>\n";
+		print "img <input type=text value=\"<img src='{$fulfil}'>\" size=10 class=\"lnkbox\"><br>\n";
+
+		print "<br>\n";
+		# make the image (downscaled) and link it to the real image
+		print "<a href=\"$fulfil\" alt=\"link to image:{$fil}\" title=\"f:{$fil} s:{$sz}\" \"><img border=0 src=\"$fil\" width=\"{$w}px\" height=\"{$h}px\"></a>" . "<br>\n";
+
+		print "</td>";
+
+		if( ($cell % 4) == ($perrow-1) ) {
+			print "</tr>\n";
+		}
+		$cell++;
 	}
-	else
-	{
-		$w = $data[0];
-	}
-
-	if($h < $minh)
-	{
-		$h = $minh;
-		$w = floor($h * $rat);
-		if($w > 300) {$w=300;}
-	}
-	else
-	{
-
-	}
-
-	//get some info prepared for use
-	$fulfil = ($ppath.$fil);
-	$sz = size_readable (filesize($fil));
-
-	//print the name at the top
-	print "<span class=\"fil\">$fil</span><br>\n";
-
-	//*****not modded  WxH****
-	print "<span class=\"siz\">{$data[0]}&times;{$data[1]}</span><br>\n";
-	//print $sz . "<br>\n";
-
-	print "</div>\n";
-
-	print "raw <input type=text value=\"".$fulfil."\" size=10 class=\"lnkbox\"><br>\n";
-	print 'link <input type=text value="'."<a href='{$fulfil}'>text</a>".'" size=10 class="lnkbox">'."<br>\n";
-	print "img <input type=text value=\"<img src='{$fulfil}'>\" size=10 class=\"lnkbox\"><br>\n";
-
-	print "<br>\n";
-	//make the image (downscaled) and link it to the real image
-	print "<a href=\"$fulfil\" alt=\"link to image:{$fil}\" title=\"f:{$fil} s:{$sz}\" \"><img border=0 src=\"$fil\" width=\"{$w}px\" height=\"{$h}px\"></a>" . "<br>\n";
 
 
-	print "</td>";
-
-
-	if(($cell % 4) == ($perrow-1))
-	{ print "</tr>\n"; }
-	$cell++;
-}
-
-
-print "</table>\n";
-/************************ END MAIN LOGIC*********************/
+	print "</table>\n";
+	#************************ END MAIN LOGIC*********************
 
 }
 
 ?>
 <hr width="25%">
 <center><small style="color: #4f4f4f; font-family: Verdana;">
-<? print " pass:[{$passc}] fail:[{$failc}] " .(($banc>0)?("ban: {$banc}"):("")) ."\n"; ?><br>
-&copy; 2007 c. stafford<br>
+<?php print " pass:[{$passc}] fail:[{$failc}] " .(($banc>0)?("ban: {$banc}"):("")) ."\n"; ?><br>
+&copy; 2007-2014 c. 'uberfuzzy' stafford<br>
 </small></center>
-</body><?
-/*************************FUNC********************************/
-function c($c,$t)
-{return "<font color=\"$c\">$t</font>"; }
+</body><?php
+#*************************FUNC********************************
+function c($c,$t) {
+	return "<font color=\"$c\">$t</font>";
+}
 
-function filter($raw, $output=0)
-{
+function filter($raw, $output=0) {
 	global $passc;
 	global $passc;
 	global $failc;
 
-	//list of crap to NOT display
+	#list of crap to NOT display
 	$vorbotten = array("*.php");
 
-	foreach($raw as $index=>$f)
-	{
-		foreach($vorbotten as $BAN)
-		{
-			if( fnmatch($BAN, $f) )
-			{
+	foreach( $raw as $index => $f ) {
+		foreach( $vorbotten as $BAN ) {
+			if( fnmatch($BAN, $f) ) {
 				$banc++;
-				if($output){print c('yellow','BAN')." [$BAN][$f]<br>\n";}
+				if( $output ) {
+					print c('yellow','BAN')." [$BAN][$f]<br>\n";
+				}
 				unset($raw[$index]);
 				continue 2;
 			}
 		}
 	}
 
-
 	$ext = array("jpg", "jpeg", "gif", "png");
-	foreach($raw as $f)
-	{
+	foreach( $raw as $f ) {
 		$dump = true;
-		foreach($ext as $e)
-		{
-			if( fnmatch("*.$e", strtolower($f)) )
-			{
+		foreach( $ext as $e ) {
+			if( fnmatch("*.$e", strtolower($f)) ) {
 				$dump = false;
 				$clean[] = $f;
 				$passc++;
 				if($output){print c('lime','pass')." [$e][$f]<br>\n";}
 				continue 2;
-			}
-			else
-			{
-				//code used to be here, but now its not, so carry on
+			} else {
+				#code used to be here, but now its not, so carry on
 			}
 		}
 
-		if($dump)
-		{
-			if($output){print c("red","fail?") . " [$f]<br>\n";}
+		if( $dump ) {
+			if( $output ) {
+				print c("red","fail?") . " [$f]<br>\n";
+			}
 			$failc++;
 		}
 	}
@@ -173,7 +160,7 @@ function filter($raw, $output=0)
 }
 
 function size_readable ($size, $retstring = null) {
-	// adapted from code at http://aidanlister.com/repos/v/function.size_readable.php
+	# adapted from code at http://aidanlister.com/repos/v/function.size_readable.php
 	$sizes = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
 	if ($retstring === null) { $retstring = '%01.2f %s'; }
 	$lastsizestring = end($sizes);
@@ -181,8 +168,8 @@ function size_readable ($size, $retstring = null) {
 		if ($size < 1024) { break; }
 		if ($sizestring != $lastsizestring) { $size /= 1024; }
 	}
-	if ($sizestring == $sizes[0]) { $retstring = '%01d %s'; } // Bytes aren't normally fractional
+	if ($sizestring == $sizes[0]) { $retstring = '%01d %s'; } # Bytes aren't normally fractional
 	return sprintf($retstring, $size, $sizestring);
 }
-/***********************end func*****************************/
+#***********************end func*****************************
 ?>
